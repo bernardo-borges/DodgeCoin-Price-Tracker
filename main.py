a73ce4c2656da1352b841679b88584bb11c6d008
Bernardo <bernardo_filipe89@hotmail.com>
2021-05-10 19:59:45 +0100

    changes

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import yfinance as yf
-import numpy as np
-import pandas as pd
-import matplotlib as mpl
-import matplotlib.pyplot as plt
 import plotly.express as px
 import datetime as date
 
-dateStart = date.datetime.now() - date.timedelta(days=2*365)
-dateToday = date.datetime.now().strftime("%Y-%m-%d")
-# Pick ticker symbol of our stock
+
+
 tkr_str = 'DOGE-EUR'
 tkr = yf.Ticker(tkr_str)
+dateStart = date.datetime.now() - date.timedelta(days=1*365)
+dateToday = date.datetime.now().strftime("%Y-%m-%d")
+graph  =  tkr.history(start=dateStart,end=dateToday,interval="1h")
 
-graph  =  tkr.history(start=dateStart,end=dateToday)
+
 graph.head()
 graph = graph.reset_index()
-fig = px.line(graph, x="Date", y="Open", title='Doge Price Tracker')
+fig = px.line(graph, x="index", y="Open", title='Doge Price Tracker')
 fig.show()
